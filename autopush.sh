d22b0d315743ebe243251326c404758cb6bada32
ruihan11 <Abc20140120@outlook.com>
2024-05-11 10:18:08 +0800

    Automatic commit message

--- autopush.sh
+++ autopush.sh
@@ -1,12 +1,14 @@
+
+rm task.png
 #github synchronize
 git add .
 git commit -m "Automatic commit message"
 git push origin main
 
 #generate png and display on windows desktop
-pandoc task.md -o task.html
-wkhtmltoimage --encoding "UTF-8" task.html task.png
+sed -n '1,30p' task.md | pandoc -f markdown -t html -o task.html
+node html2png.js
+
 /mnt/d/your_dir_to_rainmeter/Rainmeter.exe \!Refresh "Calendar"
 
 rm task.html
-rm task.png
